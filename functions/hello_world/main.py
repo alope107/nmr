0e824bc8e610c3d1a4b211a6f7b5c56330de25d1
John Roe <fake@example.com>
2019-07-12 20:57:36 -0700

    Testing drive access

--- functions/hello_world/main.py
+++ functions/hello_world/main.py
@@ -1,17 +1,15 @@
+from googleapiclient.discovery import build
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+service = build('drive', 'v3')
+
 def hello_world(request):
-    """Responds to any HTTP request.
+    """Lists the files available to the service account.
     Args:
         request (flask.Request): HTTP request object.
     Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+        A string enumerating the available files.
     """
-    resp = "hi"
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        resp = request.args.get('message') + " sup"
-    elif request_json and 'message' in request_json:
-        resp = request_json['message'] + " yo"
-    print(resp)
-    return resp
+    
+    results = service.files().list(pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    res = str(results.get('files', []))
+    return res
